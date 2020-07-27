c4a668b03f4e02837b0ffcb15ca50545f7e55725
TSTman <tstman@protonmail.com>
2020-07-26 19:39:36 -0600

    Extract PrimeTimeButton class

--- src/KeyboardHaver.java
+++ src/KeyboardHaver.java
@@ -202,40 +202,4 @@ public class KeyboardHaver extends JFrame implements KeyListener {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(KeyboardHaver::showStuff);
     }
-
-    private static class PrimeTimeButton {
-        final String name;
-        final int keyCode;
-        final int xPosition;
-        final int yPosition;
-        final int slot;
-
-        public PrimeTimeButton(final String name, final int keyCode, final int xPosition, final int yPosition, final int slot) {
-            this.name = name;
-            this.keyCode = keyCode;
-            this.xPosition = xPosition;
-            this.yPosition = yPosition;
-            this.slot = slot;
-        }
-
-        public int getSlot() {
-            return slot;
-        }
-
-        public int getKeyCode() {
-            return keyCode;
-        }
-
-        public int getXPosition() {
-            return xPosition + RandomInt(-10, 10);
-        }
-
-        public int getYPosition() {
-            return yPosition + RandomInt(-10, 10);
-        }
-
-        protected int RandomInt(int min, int max) {
-            return min + (int) (Math.random() * (max - min) + 1);
-        }
-    }
 }
