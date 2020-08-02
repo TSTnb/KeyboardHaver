46d694821f2792ef8b74307b20a7e298c95b557e
TSTman <tstman@protonmail.com>
2020-08-02 16:45:45 -0600

    Unused import

--- app/src/main/java/com/tstman/keyboardhaver/KeyboardHaverService.java
+++ app/src/main/java/com/tstman/keyboardhaver/KeyboardHaverService.java
@@ -8,7 +8,6 @@ import android.view.accessibility.AccessibilityEvent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
