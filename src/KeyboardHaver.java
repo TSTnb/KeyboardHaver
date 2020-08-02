11caf1ceb7d2c2b5d322b7670c2d3e766642dab3
TSTman <tstman@protonmail.com>
2020-08-02 15:49:06 -0600

    Unused import

--- src/KeyboardHaver.java
+++ src/KeyboardHaver.java
@@ -4,7 +4,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
